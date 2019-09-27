6f4dcd859258ed408fd8120efb5215e9a2bbd5c8
Ryan <imacube@gmail.com>
2019-09-26 20:43:30 -0700

    Initial thermostat module with CRC calculation method

---
+++ thermostat/test_crc.py
@@ -0,0 +1,18 @@
+from thermostat.crc import crc_calc
+
+
+class TestCrc():
+    def test_crc_success(self):
+        """Test TurnOff.crc_calc"""
+
+        temp_setting = 1
+        heat = 2
+        cool = 3
+        fan_mode = 4
+        other_data = 5
+
+        settings_to_send = bytearray([temp_setting, heat, cool, fan_mode, other_data])
+        crc = crc_calc(settings_to_send)
+        crc = bytearray([crc])
+
+        assert bytearray(b'\xa9') == crc
