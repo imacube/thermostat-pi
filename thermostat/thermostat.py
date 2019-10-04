83360ffc7697541c3f07c25c242be26b47f75902
Ryan <imacube@gmail.com>
2019-10-04 15:14:05 -0700

    Tests and code for Thermostat.send_state

--- thermostat/thermostat.py
+++ thermostat/thermostat.py
@@ -6,7 +6,7 @@ from time import sleep
 from digi.xbee.exception import TimeoutException
 
 from thermostat.crc import crc_calc
-from thermostat.exceptions import CrcVerificationFailure, RetryException
+from thermostat.exceptions import CrcVerificationFailure, RetryException, SendFailure
 
 LOGGER = logging.getLogger(__name__)
 
@@ -67,13 +67,48 @@ class Thermostat:
                 sleep(retry_sleep)
                 continue
 
-            if crc_calc(xbee_message.data[2:]) == xbee_message.data[1]:
-                LOGGER.info('CRC match')
-            else:
+            if crc_calc(xbee_message.data[2:]) != xbee_message.data[1]:
                 LOGGER.error('CRC does not match! Try again...')
                 raise CrcVerificationFailure
+
+            LOGGER.info('CRC match')
+            break
 
         if xbee_message is None:
             raise RetryException
 
         return xbee_message
+
+    def send_state(self, data_to_send, attempts=10, retry_sleep=7):
+        """Send a new state config to the thermostat.
+
+        Parameters
+        ----------
+        data_to_send
+            Data to send to the remote device.
+        attempts : int
+            Number of times to try to send the data.
+        retry_sleep : int
+            How many seconds to wait between tries.
+
+        Raises
+        ------
+        SendFailure
+            If there is a failure to send the data.
+        """
+
+        result = None
+
+        for _ in range(attempts):
+            LOGGER.info("Sending data to %s -> %s..." % (self.remote_device.get_64bit_addr(), data_to_send))
+            try:
+                result = self.device.send_data(self.remote_device, data_to_send)
+                break
+            except Exception as exception:
+                LOGGER.error(exception)
+                sleep(retry_sleep)
+
+        if result is None:
+            raise SendFailure
+
+        return result
